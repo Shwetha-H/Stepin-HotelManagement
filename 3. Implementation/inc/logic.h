d4ceff8dd1a36b8615127acebf964cdbae79f3ac
Shwetha-H <89725788+Shwetha-H@users.noreply.github.com>
2021-09-07 17:26:03 +0530

    Add files via upload

---
+++ 3. Implementation/inc/logic.h
@@ -0,0 +1,16 @@
+#ifndef LOGIC_H_
+#define LOGIC_H_
+
+int addition();
+int subtraction();
+int multiplication();
+int division();
+int modulus();
+int factorial();
+int power();
+int square();
+int cube();
+int squareroot();
+
+#define MACRO -99
+#endif
